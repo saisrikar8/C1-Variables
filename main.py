00c16f391a2e91a6b22a15fe3ec33ece6d2d5d68
Tummala Sai <110589@eesd.org>
2021-01-12 00:31:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Variables, containers, hold a value
+# Data Type/ Variable Types
+#String (str) : Combination of characters (letters, numbers, or symbols) must usee quotation marks
+# Integer (int) : whole numbers
+# Character (char) : single letter, number, or symbol
+#Float : numbers with decimals
+#Boolean (bool) : True/False
+
+#formula :
+#variableName = value 
+# Rules for variable names:
+# 1. Always start witha letter
+# 2. They can only contain letters and numbers
+# 3. If more than word is used, use camelCase (first word is lowercase and every word after that is uppercase)
+
+firstName = "Sai" #String type
+
+favoriteBook = "'Spy School' by Stuart Gibbs"
+favNumber = "8"
+isHungry = True
+favChar = "!"
